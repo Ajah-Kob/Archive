ff781325d219958b8549071fbafd5890e7afb78e
Aaron Jacob Gutierrez <gutierrezaaron291@gmail.com>
2026-07-24 20:55:21 +0800

    chore: remove duplicate import of prisma in guard.ts

--- lib/actions/guard.ts
+++ lib/actions/guard.ts
@@ -1,7 +1,6 @@
 import { getServerSession, Session } from 'next-auth'
 import { authOptions } from '@/lib/authOptions'
 import prisma from '@/lib/prisma'
-import prisma from '@/lib/prisma'
 
 const ADMIN_ROLES = ['SUPERADMIN', 'ADMIN']
 

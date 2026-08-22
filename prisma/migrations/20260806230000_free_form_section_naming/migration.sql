2d1fd5beee04f580c0a9f229f4f77305aa32d99c
Aaron Jacob Gutierrez <gutierrezaaron291@gmail.com>
2026-08-22 11:58:34 +0800

    chore(db): squash migrations into 0_init baseline

--- prisma/migrations/20260806230000_free_form_section_naming/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "Section_section_key";
-
--- AlterTable
-ALTER TABLE "Section" DROP COLUMN "yearLevel";
-
--- CreateIndex
-CREATE UNIQUE INDEX "Section_coordinatorId_section_key" ON "Section"("coordinatorId", "section");
